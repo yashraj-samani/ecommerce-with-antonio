ca306f68b9c4cfb91d001180cd7e76b22b1461f0
MoodyCoding <yashrajsamani@gmail.com>
2025-02-19 12:40:31 +0530

    Day 6 - completed 6 hours

---
+++ ecommerce-admin/app/api/[storeId]/sizes/route.ts
@@ -0,0 +1,82 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+import prismadb from "@/lib/prismadb";
+
+//our api to creating a billboard is ready
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    //using clerk to authenticate this post route
+    const { userId } = await auth();
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    //checking both the required fields
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const size = await prismadb.size.create({
+      data: {
+        name,
+        value,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZES_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+//our api to get a billboard
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const sizes = await prismadb.size.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(sizes);
+  } catch (error) {
+    console.log("[SIZES_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
