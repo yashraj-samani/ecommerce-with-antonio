905c1630628a9305afd45c441baecaafcd5b58e9
MoodyCoding <yashrajsamani@gmail.com>
2025-02-20 11:37:50 +0530

    Day 7 - completed 7 hours

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -16,10 +16,10 @@ import {
 import { Button } from "@/components/ui/button";
 import { AlertModal } from "@/components/modals/alert-modal";
 
-import { SizeColumn } from "./columns";
+import { BillboardColumn } from "./columns";
 
 interface CellActionProps {
-  data: SizeColumn;
+  data: BillboardColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -31,17 +31,19 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Size Id copied to the clipboard.");
+    toast.success("Billboard Id copied to the clipboard.");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
       router.refresh();
-      toast.success("Size deleted.");
+      toast.success("Billboard deleted.");
     } catch (error) {
-      toast.error("Make sure you removed all products using this size first.");
+      toast.error(
+        "Make sure you removed all categories using this billboard first."
+      );
     } finally {
       setLoading(false);
       setOpen(false);
@@ -70,7 +72,9 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
             Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
+            onClick={() =>
+              router.push(`/${params.storeId}/billboards/${data.id}`)
+            }
           >
             <Edit className="mr-2 h-4 w-4" />
             Update
---
+++ ecommerce-admin/app/api/[storeId]/colors/route.ts
@@ -0,0 +1,82 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+import prismadb from "@/lib/prismadb";
+
+//our api to creating a billboard is ready
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    //using clerk to authenticate this post route
+    const { userId } = await auth();
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    //checking both the required fields
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const color = await prismadb.color.create({
+      data: {
+        name,
+        value,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLORS_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+//our api to get a billboard
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const colors = await prismadb.color.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(colors);
+  } catch (error) {
+    console.log("[COLORS_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
